3c4fc4fcba49664c0f6e20bf0d0bb121fe4d5e54
max5961 <mmorrissey5961@gmail.com>
2023-06-10 17:01:28 -0400

    Create new project form

--- src/index.js
+++ src/index.js
@@ -10,6 +10,40 @@ document.querySelector('#maximize-sidebar').onclick = () => {
     document.querySelector('.sidebar.minimized').classList.remove('visible');
 }
 
-document.querySelector('.new-project-container').onclick = () => {
-    
+document.querySelector('.new-project-modal input#project-name').oninput = (e) => {
+
+    const createButton = document.querySelector('.new-project-modal button[type=submit]');
+    const names = ['Project 1', 'Project 2'];
+    let current = e.target.value.trimEnd();
+
+    if (e.target.value.length > 0) {
+        createButton.classList.add('not-empty');
+
+        if (!names.includes(current)) {
+            e.target.classList.add('valid');
+        } else {
+            e.target.classList.remove('valid');
+        }
+
+    } else {
+        createButton.classList.remove('not-empty');
+        e.target.classList.remove('valid');
+    }
+}
+
+document.querySelector('.new-project-modal').onclick = (e) => {
+    const form = document.querySelector('.new-project-modal form');
+    const modal = document.querySelector('.new-project-modal');
+
+    if (e.target === form) {
+        return;
+    }
+
+    if (e.target === modal) {
+        modal.remove();
+    }
+}
+
+document.querySelector('button.new-project-container').onclick = () => {
+    console.log('test');
 }
