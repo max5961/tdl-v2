773e437f59319c7a7f9c92e3051d409bf0d1e72a
max5961 <mmorrissey5961@gmail.com>
2023-06-09 21:24:56 -0400

    Restructure HTML/CSS

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import './style/index.css';
 
-document.querySelector('.minimize-sidebar').onclick = () => {
-    document.querySelector('.sidebar-container').classList.add('hidden');
-    document.querySelector('.sidebar-minimized').classList.add('visible');
+document.querySelector('#minimize-sidebar').onclick = () => {
+    document.querySelector('.sidebar.maximized').classList.add('hidden');
+    document.querySelector('.sidebar.minimized').classList.add('visible');
 }
 
-document.querySelector('.maximize-sidebar').onclick = () => {
-    document.querySelector('.sidebar-container').classList.remove('hidden');
-    document.querySelector('.sidebar-minimized').classList.remove('visible');
+document.querySelector('#maximize-sidebar').onclick = () => {
+    document.querySelector('.sidebar.maximized').classList.remove('hidden');
+    document.querySelector('.sidebar.minimized').classList.remove('visible');
 }
 
 document.querySelector('.new-project-container').onclick = () => {
